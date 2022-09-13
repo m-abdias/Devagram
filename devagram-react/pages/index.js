2d739761f52c01626d2eb2c6d3207d4407beb0ae
mariana abdias goncalves <mabdiasgoncalves@gmail.com>
2022-09-13 15:08:56 -0300

    Importado svg, criado avatar e botões

--- devagram-react/pages/index.js
+++ devagram-react/pages/index.js
@@ -1,3 +1,18 @@
+import Avatar from '../components/avatar'
+import Botao from '../components/botao'
+
 export default function Home() {
-  return <h1>Devagram</h1>
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <div style={{ width: 200 }}>
+        <Avatar />
+        <Botao
+          texto={'login'}
+          cor="invertido"
+          manipularClique={() => console.log('botão clicado')}
+        />
+      </div>
+    </>
+  )
 }
